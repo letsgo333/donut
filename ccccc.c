1dffdb2ff32674063ae741946762a513d77e2417
letsgo333 <58031767+letsgo333@users.noreply.github.com>
2020-04-07 01:28:22 -0600

    Update ccccc.c

--- ccccc.c
+++ ccccc.c
@@ -1,15 +1,21 @@
-##include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <io.h>
+
 
 int main(int argc, char * argv[]) {
 	
 	if (argv[1]!=NULL && argv[2] !=NULL ){
 		//Create New shellcode file 
+		DecodeCreateNewFile(argv[1],argv[2]);
 		
 		printf("Create new file :%s\n",argv[2]);
 		return 0;
 	}else if (argv[1] != NULL && argv[2] == NULL){
 		//Run shellcode filename:a1
+		
+		RunCode(DecodeCreateNewFile(argv[1]));
 		
 		printf("Run shellcode file :%s",argv[1]);
 		return 0;
@@ -19,57 +25,70 @@ int main(int argc, char * argv[]) {
 	}
 	
 
-//	int a = 64;
-//	int b = 1169;
-//	int c = a^b;
-	//1169
-//	printf("a^b = %d",c);
-	
 	return 0;
 }
 
 
 //Create new Shellcode File
-void CreateNewFile(char * oldfile ,char * newfile){
+void EncodeCreateNewFile(char * oldfile ,char * newfile){
 	//read old file
-	int key = 249685;
-	FILE * oldf,newf;
-	oldf = fopen(oldfile,"rb");
-	newf = fopen(newfile,"wb");
-	unsigned char buf[MAXLEN];
-	if(oldf == NULL || newf == NULL){
-		exit(1);
-	}
-	
-	int rc;
-	//here need oldf stream ^ key
-	while((rc=fread(buf,sizeof(unsigned char),MAXLEN,oldf))!=0){
-		fwrite(buf,sizeof(unsigned char),rc,newf);
-	}
-	fclose(oldf);
-	fclose(newf);
-	return 0;
+	int keylen, index=0;
+    char *source, *dest, *key, fBuffer[1], tBuffer[20], ckey;
+    FILE *fSource, *fDest;
+    source = oldfile; // 原文件
+    dest = newfile;   // 目的文件
+    key = "rmvpfr";    // 加密字串
+    // 获取key长度
+    keylen = strlen(key);
+ 
+    fSource = fopen(source, "rb");
+    fDest = fopen(dest, "wb");
+ 
+    while(!feof(fSource)){
+        
+        fread(fBuffer, 1, 1, fSource);    // 读取1字节
+        
+        if(!feof(fSource)){
+            ckey = key[index%keylen];     // 循环获取key
+            *fBuffer = *fBuffer ^ ckey;   // xor encrypt
+            fwrite(fBuffer, 1, 1, fDest); // 写入文件
+            index ++;
+        }
+    
+    }
+ 
+    fclose(fSource);
+    fclose(fDest);
+    exit(0);
+
+}
+
+void DecodeCreateNewFile(char * oldfile){
+	//decode file
+	int keylen, index=0;
+    char *source, *key, fBuffer[1], tBuffer[20], ckey;
+    FILE *fSource;
+    source = oldfile;
+    key = "rmvpfr";
+    keylen = strlen(key);
+    fSource = fopen(source, "rb");
+    while(!feof(fSource)){
+        fread(fBuffer, 1, 1, fSource);
+        if(!feof(fSource)){
+            ckey = key[index%keylen];	//get key
+            *fBuffer = ckey ^ *fBuffer;	//xor decrypt
+            index ++;
+        }
+    }
+    fclose(fSource);
+ 	return *fBuffer;
 }
 
 
 void RunCode(char * file){
-	//Run file
-	
-	
+	//Run file load shellcode to RAM
+	printf("Run success");
+	return 0;
 }
-
-
-void decodefile(char * file){
-	//here need newf key ^ newf stream
-	
-	
-}
-
-
-
-
-
-
-
 
 
